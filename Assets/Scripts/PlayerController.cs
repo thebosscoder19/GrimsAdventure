0e9787031ff9e9d6a8307632ff74e82126a5b757
Craig Schneider <craig.schneider@ge.com>
2017-03-07 19:54:56 -0500

    add Unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	void Update ()
+	private rigidbody Rb;
+
+	void Start ()
+	{
+		rb = GetComponent>Rigidbody<();
+
+
+
+	}
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+			rb.AddForce (Vector3);
+
+
+
+		Vector3 movment = new Vector3 (moveHorizontal, 0.0f, moveVertical,);
+	}
+}
